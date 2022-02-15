39577bc4538f7127f6477d0a7320d4a1ae5cab10
SirEliaas <sreliaas@gmail.com>
2022-02-14 21:35:02 -0300

    :rocket: first commit

---
+++ src/screens/MainScreen/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {View} from "react-native";
+import styles from "./styles";
+
+export const MainScreen: React.FC = () => {
+  return (
+    <View></View>
+  );
+};
---
+++ src/screens/ViewProductScreen/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {View} from "react-native";
+import styles from "./styles";
+
+export const ViewProductScreen: React.FC = () => {
+  return (
+    <View></View>
+  );
+};
